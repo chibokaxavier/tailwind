2d98ac5f18a5fd03e4cb31725d69282251f85ff9
Xavier <chibokaxavier@gmail.com>
2022-06-29 04:36:45 -0700

    finished

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        head:'#20232a',
+        bluee:'#61dafb',
+        bodyblack:'#282c34',
+        h3color:'#6d6d6d',
+        codecolor:'rgba(255,229,100,0.2)',
+        codecolor2:'rgba(187,239,253,0.3)',
+        hover:'#bbeffd',
+        h3: '#282c34',
+        linkback:'#282c34',
+        footercolor:'#20232a',
+        facebook:'#999',
+      }
+    },
+  },
+  plugins: [],
+}
